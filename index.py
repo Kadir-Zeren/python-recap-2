d34bc48ceccc748263cc47b310b86c22c182cc25
kadir-zeren <k4dzeren@gmail.com>
2024-02-17 15:49:22 +0300

    variables

--- index.py
+++ index.py
@@ -1,1 +1,56 @@
 print('Hello')
+# bu bir comment
+# bu ikinci comment
+# bu ucuncu comment
+
+a = 'bu benim yazdigim string \
+    stringe devam ediyorum'
+print(a)
+
+a= 'bu benim yazdigim string \nstringe devam ediyorum'
+print(a)
+
+
+school = "clarusway"
+print(school)
+sayi = 35
+print(sayi)
+
+ggg = 5 
+
+print(ggg)
+
+a = "merhaba"
+b="python"
+print(a,b)
+
+
+my_age =33
+your_age =30
+my_age =your_age
+print(my_age)
+
+okul = "Clarusway"
+print(okul)
+
+variable = 3.14
+var_one="something"
+
+print(help('keywords'))
+
+degisken = "bu bir string"
+print(degisken)
+
+degisken = 5
+print(degisken)
+
+x = y = z ="same"
+print(x)
+print(y)
+print(z)
+
+a = 'degisken_1'
+b = 5
+c = 7.6
+
+print(a,b,c)
